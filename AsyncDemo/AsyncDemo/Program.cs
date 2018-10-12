d3f8d9dd340657a7135776ba69f7f053403176a4
Zhang Edward <hhuhszy@outlook.com>
2018-10-12 20:49:46 +0800

    c#

---
+++ AsyncDemo/AsyncDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace AsyncDemo
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new Application();
+            app.Run(new MyWindow());
+        }
+    }
+}
