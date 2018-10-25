db9323c6b37daf1103518c314f6486f3ca94dddd
Zhang Edward <hhuhszy@outlook.com>
2018-10-25 23:04:12 +0800

    1

---
+++ CSharpExercise/DemoLibrary.Test/DemoLibraryTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DemoLibrary.Test
+{
+    public class DemoLibraryTests
+    {
+        [Theory]
+        [MemberData(nameof(AddShouldCalcGetData),parameters:10)]
+        public void AddShouldCalc(double x, double y, double expected)
+        {
+            //Arrange
+            //static class
+            //Act
+            var actual = Calculator.Add(x, y);
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        public static IEnumerable<object[]> AddShouldCalcGetData(int num)
+        {
+            var result = new List<object[]>();
+            for (int i = 0; i < num; i++)
+            {
+                result.Add(new object[] { i, i + 6, 4 * i });
+            }
+            return result;
+        }
+
+
+        [Fact]
+        public void ExampleLoadTextFileShouldWork()
+        {
+            var str = Examples.ExampleLoadTextFile("this is a valid path");
+
+            Assert.True(str.Length > 0);
+        }
+
+        [Fact]
+        public void ExampleLoadTextFileShouldThrowException()
+        {
+            Assert.Throws<System.IO.FileNotFoundException>(() =>
+            {
+                Examples.ExampleLoadTextFile("");
+            });
+        }
+    }
+}
---
+++ CSharpExercise/DemoLibrary/Examples.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoLibrary
+{
+    public static class Examples
+    {
+        public static string ExampleLoadTextFile(string path)
+        {
+            if (path.Length < 10)
+            {
+                throw new System.IO.FileNotFoundException();
+            }
+
+            return "The file is loaded successfully";
+        }
+    }
+}
